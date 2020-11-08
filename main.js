e22c954afd1a35e95d1b8dd753897cebc157fe8a
malikabakhtiyorova <malikabakhtiyorova@yandex.ru>
2020-11-08 20:28:51 +0500

    Initial-commit

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("Assalamu alaykum va rohmatullohi va barokatuh! Marhamat qilib shahsiy malumotlarni kiriting. Quyida kiritilgan malumotlar havfsizlik tomonidan himoyalangan va sir tutiladi.");
+
+var name = prompt("Ismingizni kiriting(kunya kiritsa ham bo'ladi):");
+var lastName = prompt("Familyangizni kiriting:");
+var age = prompt("Yoshizni kirting:");
+var job = prompt("Ta'lim/ kasb ni kirting:");
+var telegram = prompt("Telegram link kirting(bu ihtiyoriy):");
+
+console.log("Yangi mijoz!" + "\nIsm-familya: " + name + " " + lastName + "\nYosh: " + age + "\nKasb-ta'lim: " + job + "\nTelegram profil: " + telegram + "\n" + "\nHayrli bo'lsin!")
